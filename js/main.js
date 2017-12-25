abaf6fffadb19d77ce7dabee625f786e5f48a6c9
Noman <noman@noman.land>
2017-12-24 21:33:29 -0500

    Make simple browser example with webpack

---
+++ js/main.js
@@ -0,0 +1,60 @@
+import IPFS from 'ipfs';
+import '../sass/style.sass';
+
+const node = new IPFS();
+
+const PUBLIC_GATEWAY = 'https://ipfs.io/ipfs';
+
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(() => {
+  function addText(text) {
+    return new Promise((resolve, reject) => {
+      node.files.add(new Buffer(text), (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+
+        return resolve(result);
+      });
+    })
+  }
+
+  function logSuccess([{ hash }]) {
+    console.log('\nNice! Here\'s your hash:\n', hash);
+    const url = `${PUBLIC_GATEWAY}/${hash}`;
+    const link = document.createElement('a');
+    const span = document.createElement('span');
+    const resultDiv = document.getElementsByClassName('result')[0];
+
+    span.innerText = 'Find your content at ';
+    link.href = link.innerText = url;
+    link.target = '_blank';
+
+    resultDiv.innerHTML = '';
+
+    resultDiv
+      .appendChild(span)
+      .appendChild(link);
+  }
+
+  function logError(error) {
+    console.error('\nUh oh!\n', error);
+  }
+
+  document.getElementById('add-stuff').addEventListener('submit', event => {
+    event.preventDefault();
+    const text = document.getElementById('stuff').value;
+    addText(text).then(logSuccess, logError);
+  });
+
+  node.on('ready', () => {
+    console.log('Connected to IPFS!');
+  });
+});
---
+++ webpack.config.prod.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const config = {
+  entry: [
+    path.resolve(__dirname, 'js/main.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'static/bundle'),
+    publicPath: '/static/bundle',
+    filename: 'main.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.json$/,
+        loaders: ['json-loader']
+      },
+      {
+        test: /\.sass$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
+    ]
+  }
+};
+module.exports = config;
